ad52bff996b787858de0e0873df506a06d3b4349
Pete Lewis <pete.lewis@sparkfun.com>
2016-09-08 15:32:57 -0600

    Programming stuff for use with Pi_grammer
    -contains bash files used on stand alone raspi programmer
    -separate bash files for fuse/boot/lock, and serial upload
    -contains bootloader hex and sefl-test hex
    -also has most current test.py python script. This will most likely get
    augmented, in the Pi_grammer repo, so don't trust this copy is up to
    date. visit repo for the latest and greatest:
    https://github.com/lewispg228/Pi_grammer

---
+++ PRODUCTION_TESTING_STUFF/Pi_grammer_stuff/pi_program.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "Programming beginning..."
+
+# get firmware file name
+firmware=$(find /home/pi/*.hex)
+$firmware .= "/home/pi/$firmware"
+
+#FUSE BITS
+HIGH_FUSE=0xD8
+LOW_FUSE=0xFF
+EXT_FUSE=0x05 # due to masking, 0x05 = 0xFD (FD was the original ext fuse for the mega pro
+LOCK=0x0F
+
+#to verify that avrdude can connect to the chip
+#sudo avrdude -p atmega328p -C ~/avrdude_gpio.conf -c pi_1 -v
+
+#erase and fuse bits, note the -i 100 to add delay and slow down initial com to fresh chips
+sudo avrdude -p atmega2560 -C /home/pi/avrdude_gpio.conf -c pi_1 -D -v -e -u -U hfuse:w:$HIGH_FUSE:m -u -U lfuse:w:$LOW_FUSE:m -u -U efuse:w:$EXT_FUSE:m 2>/home/pi/fuse_results.txt
+
+#program flash and lock bits
+# extended should be 0xFD, but they only verify at 0x05, 
+# also lock fuses should be 0xCF, but they only verify at 0x0F
+sudo avrdude -p atmega2560 -C /home/pi/avrdude_gpio.conf  -c pi_1 -D -v -u -U flash:w:$firmware:i -u -U lock:w:$LOCK:m 2>/home/pi/flash_results.txt
+
+
