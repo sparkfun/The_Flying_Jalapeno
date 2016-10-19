abb16d6d8b58de0fe1eec94c2e1b4b5af25eda71
Pete Lewis <pete.lewis@sparkfun.com>
2016-10-19 10:19:28 -0600

    added -i delay to fuse programming, changed EXT_FUSE to FD
    to ensure proper speed for fresh chip.
    
    Mega2560 verifies a FD when you write an FD to ext fuse

--- PRODUCTION_TESTING_STUFF/Pi_grammer_stuff/pi_program.sh
+++ PRODUCTION_TESTING_STUFF/Pi_grammer_stuff/pi_program.sh
@@ -9,14 +9,14 @@ $firmware .= "/home/pi/$firmware"
 #FUSE BITS
 HIGH_FUSE=0xD8
 LOW_FUSE=0xFF
-EXT_FUSE=0x05 # due to masking, 0x05 = 0xFD (FD was the original ext fuse for the mega pro
+EXT_FUSE=0xFD # due to masking, 0x05 = 0xFD (FD was the original ext fuse for the mega pro) *** Note, the mega verifies back as FD
 LOCK=0x0F
 
 #to verify that avrdude can connect to the chip
 #sudo avrdude -p atmega328p -C ~/avrdude_gpio.conf -c pi_1 -v
 
 #erase and fuse bits, note the -i 100 to add delay and slow down initial com to fresh chips
-sudo avrdude -p atmega2560 -C /home/pi/avrdude_gpio.conf -c pi_1 -D -v -e -u -U hfuse:w:$HIGH_FUSE:m -u -U lfuse:w:$LOW_FUSE:m -u -U efuse:w:$EXT_FUSE:m 2>/home/pi/fuse_results.txt
+sudo avrdude -p atmega2560 -C /home/pi/avrdude_gpio.conf -c pi_1 -i 1000 -D -v -e -u -U hfuse:w:$HIGH_FUSE:m -u -U lfuse:w:$LOW_FUSE:m -u -U efuse:w:$EXT_FUSE:m 2>/home/pi/fuse_results.txt
 
 #program flash and lock bits
 # extended should be 0xFD, but they only verify at 0x05, 
